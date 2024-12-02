85336a0245fc72271090c52aed586a9cf3968f55
Michael Blättler <michael.blaettler@innetag.ch>
2024-12-02 19:03:33 +0100

    refactor(puzzle2): make code cleaner by using between method of pandas

--- day2/puzzle2.py
+++ day2/puzzle2.py
@@ -1,13 +1,12 @@
 import pandas as pd
+import numpy as np
 
 
 def count_correct_reports(data: []) -> int:
     correct_reports = 0
     for report in data:
-        diff = report.diff()
-        if diff.max() <= 3 and diff.min() > 0:
-            correct_reports += 1
-        elif diff.min() >= -3 and diff.max() < 0:
+        diff = report.diff().dropna()
+        if np.all(diff.between(1, 3)) or np.all(diff.between(-3, -1)):
             correct_reports += 1
 
     return correct_reports
